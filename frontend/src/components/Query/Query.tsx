57351ba696a044ac26f8af51200128af55e7810c
Brian McVeigh <bcmcveigh@gmail.com>
2020-10-18 11:21:59 -0400

    Added apollo Query component.

---
+++ frontend/src/components/Query/Query.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+
+interface IQuery extends Readonly<any> {
+  children: any;
+  query: any;
+  id: any;
+  variables?: any;
+}
+
+const Query: ({ children, query, id, variables }: IQuery) => (any) = ({ children, query, id, variables }) => {
+  if (!variables) {
+    variables = {};
+  }
+  if (id) {
+    variables.id = id;
+  }
+
+  const { data, loading, error } = useQuery(query, {
+    variables: variables
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {JSON.stringify(error)}</p>;
+  return children({ data });
+};
+
+export default Query;
