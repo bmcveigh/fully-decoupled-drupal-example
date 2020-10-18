2a5ca1b53a8517877acb4087a0f71d0f63da826c
Brian McVeigh <bcmcveigh@gmail.com>
2020-10-18 19:15:15 -0400

    Fix missing key prop issue with articles display.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import Query from '../components/Query/Query';
 import ARTICLES_QUERY from '../graphql/queries/articles';
+import { Typography } from '@material-ui/core';
 
 const Home = () => {
   return (
     <Query query={ARTICLES_QUERY}>
       {(results: any): any => {
         return results.data.nodeQuery.entities.map((node: any) => (
-          <div>{node.entityLabel}</div>
+          <Typography key={node.entityLabel} variant="h3">
+            {node.entityLabel}
+          </Typography>
         ));
       }}
     </Query>
