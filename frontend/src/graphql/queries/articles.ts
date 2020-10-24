c118a6d31f6536ced868d89e0576bc3e63ed0662
Brian McVeigh <bcmcveigh@gmail.com>
2020-10-24 12:13:06 -0400

    Added entityId to graphql query.

--- frontend/src/graphql/queries/articles.ts
+++ frontend/src/graphql/queries/articles.ts
@@ -5,7 +5,8 @@ const ARTICLES_QUERY = gql`
   query {
     nodeQuery(limit: 10, offset: 0, filter: {conditions: [{operator: EQUAL, field: "type", value: ["article"]}]}) {
       entities {
-        entityLabel,
+        entityId,
+        entityLabel
       }
     }
   }
