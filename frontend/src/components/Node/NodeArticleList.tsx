65c25018f8921fbd74a84bf726232a75f0549af5
Brian McVeigh <bcmcveigh@gmail.com>
2020-10-25 10:47:03 -0400

    Display node articles as a list.

---
+++ frontend/src/components/Node/NodeArticleList.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import Query from '../../components/Query/Query';
+import ARTICLES_QUERY from '../../graphql/queries/articles';
+import NodeArticleType from '../../types/NodeArticleType';
+import NodeArticlesQueryResponse from '../../types/NodeArticlesQueryResponse';
+import {
+  createStyles,
+  Divider,
+  List, ListItem,
+  ListItemText,
+  Theme,
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      backgroundColor: theme.palette.background.paper,
+    },
+    inline: {
+      display: 'inline',
+    },
+  }),
+);
+
+const NodeArticleList = () => {
+  const classes = useStyles();
+
+  return (
+    <Query query={ARTICLES_QUERY}>
+      {(results: NodeArticlesQueryResponse): any => {
+        return (
+          <List className={classes.root}>
+            {results.data.nodeQuery.entities.map((node: NodeArticleType) => (
+              <React.Fragment key={node.entityId}>
+                <ListItem alignItems="flex-start">
+                  <ListItemText
+                    primary={node.entityLabel}
+                    secondary={
+                      node.body.summary ? (
+                        <React.Fragment>
+                          {node.body.summary}
+                        </React.Fragment>
+                      ) : null
+                    }
+                  >
+                  </ListItemText>
+                </ListItem>
+                <Divider variant="inset" component="li" />
+              </React.Fragment>
+            ))}
+          </List>
+        )
+      }}
+    </Query>
+  );
+};
+
+export default NodeArticleList;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,20 +1,6 @@
 import * as React from 'react';
-import Query from '../components/Query/Query';
-import ARTICLES_QUERY from '../graphql/queries/articles';
-import NodeArticle from '../components/Node/NodeArticle';
-import NodeArticleType from '../types/NodeArticleType';
-import NodeArticlesQueryResponse from '../types/NodeArticlesQueryResponse'
+import NodeArticleList from '../components/Node/NodeArticleList';
 
-const Home = () => {
-  return (
-    <Query query={ARTICLES_QUERY}>
-      {(results: NodeArticlesQueryResponse): any => {
-        return results.data.nodeQuery.entities.map((node: NodeArticleType) => (
-          <NodeArticle node={node} key={node.entityId} />
-        ));
-      }}
-    </Query>
-  );
-};
+const Home = () => <NodeArticleList />;
 
 export default Home;
