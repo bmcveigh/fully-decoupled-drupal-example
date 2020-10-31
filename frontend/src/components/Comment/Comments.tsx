22f36547a90f4b72bab986000dc05b3e5443cc59
Brian McVeigh <bcmcveigh@gmail.com>
2020-10-31 17:42:07 -0400

    Modified comments component to use Material styles.

--- frontend/src/components/Comment/Comments.tsx
+++ frontend/src/components/Comment/Comments.tsx
@@ -1,25 +1,61 @@
 import * as React from 'react';
 
 import {
+  Avatar, createStyles, Divider,
+  List, ListItem, ListItemAvatar, ListItemText, Theme,
   Typography,
 } from '@material-ui/core';
-import EntitiesType from '../../types/EntitiesType'
+import EntitiesType from '../../types/EntitiesType';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      backgroundColor: theme.palette.background.paper,
+    },
+    inline: {
+      display: 'inline',
+    },
+  }),
+);
 
 interface Props {
   entities: EntitiesType[];
 }
 
 const Comments = (props: Props) => {
-  return (
-    <div>
-      <Typography variant="h5">Comments</Typography>
-      {props.entities ? props.entities.map(comment => (
-        <div key={comment.entityId}>
-          <Typography variant="body1">{comment.entityLabel}</Typography>
-        </div>
-      )) : null}
-    </div>
+  const classes = useStyles();
 
+  return (
+    <List className={classes.root}>
+      {props.entities ? props.entities.map(comment => (
+        <React.Fragment key={comment.entityId}>
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary={comment.entityLabel}
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    component="span"
+                    variant="body2"
+                    className={classes.inline}
+                    color="textPrimary"
+                  >
+                    Ali Connors
+                  </Typography>
+                  {comment.entityLabel}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+        </React.Fragment>
+      )) : null}
+    </List>
   );
 };
 
